bda01fb2723991210eb1001cb7c3e690a35b2b65
klgibsonjr <klgibsonjr@gmail.com>
2022-11-17 16:05:57 -0800

    dynamically added city name and current date and temperature to html

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,10 @@ const searchBtnElm = document.getElementById('search-btn');
 const searchHistoryElm = document.getElementById('search-history');
 const searchResultElm = document.getElementById('search-result');
 const forecastResultElm = document.getElementById('forecast-result');
+const cityNameElm = document.getElementById('city-name');
+const tempElm = document.getElementById('temp');
+const windElm = document.getElementById('wind');
+const humidityElm = document.getElementById('humidity');
 
 // Event listeners applied to search button
 searchBtnElm.addEventListener('click', function (event) {});
@@ -11,37 +15,45 @@ let searchResults = function () {
   const searchInputElm = document.getElementById('search-input');
   let userSearch = searchInputElm.value;
   let weatherCoordsApi = 'https://api.openweathermap.org/geo/1.0/direct';
-  weatherCoordsApi = weatherCoordsApi + '?q=' + userSearch + '&limit=&appid=f3dd875ac81e50aaada068245357b0ee';
-  // console.log(weatherCoordsApi);
+  weatherCoordsApi = weatherCoordsApi + '?q=' + userSearch + '&limit=&appid=f3dd875ac81e50aaada068245357b0ee&units=imperial';
 
   fetch(weatherCoordsApi)
     .then(function (response) {
       return response.json();
     })
     .then(function (response) {
-      console.log(response);
+      // console.log(response);
+
+      let cityName = response[0].name;
 
       let latitude = response[0].lat;
-      console.log(latitude);
-
       let longitude = response[0].lon;
-      console.log(longitude);
+
+      console.log(cityName, latitude, longitude);
 
       let weatherForecastApi = 'https://api.openweathermap.org/data/2.5/forecast';
       weatherForecastApi = weatherForecastApi + '?lat=' + latitude + '&lon=' + longitude + '&appid=f3dd875ac81e50aaada068245357b0ee';
-      console.log(weatherForecastApi);
 
       fetch(weatherForecastApi)
         .then(function (response) {
           return response.json();
         })
         .then(function (response) {
-          console.log(response.list[0].wind.speed);
+          let temp = response.list[0].main.temp;
+          let humidity = response.list[0].main.humidity;
+          let wind = response.list[0].wind.speed;
+
+          console.log(temp);
+          console.log(humidity);
+          console.log(wind);
+          console.log(response);
+
+          tempElm.textContent = 'Temp: ' + temp;
+          windElm.textContent = 'Wind: ' + wind;
+          humidityElm.textContent = 'Humidity: ' + humidity;
         });
 
-      let temp = response.list[0].main.temp;
-      let humidity = response.list[0].main.humidity;
-      let wind = response.list[0].main.wind.speed;
+      cityNameElm.textContent = cityName + ' ' + dayjs().format('MM/DD/YYYY');
     });
 };
 
